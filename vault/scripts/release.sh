dc5a80c21623f2389be1662f724111ad21bff79c
dpancu <dpancu@godaddy.com>
2020-10-19 02:20:08 +0300

    Added new 1.5.4 vault image

--- vault/scripts/release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'Start release for project: '$1 #$1 path of the project to be released
-cd $1 && git tag $2 #$2 the release tag value e.q: vX.X.X
-python3 setup.py sdist bdist_wheel
-twine upload dist/* --repository-url https://upload.pypi.org/legacy/
