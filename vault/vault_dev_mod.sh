28af9bc901270f9139678c745fadf70178acd178
dpancu <dpancu@godaddy.com>
2020-11-06 01:20:09 +0200

    Added vault dev sh

--- vault/vault_dev_mod.sh
+++ vault/vault_dev_mod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=vault' --cap-add=IPC_LOCK -d --name=dev-vault vault:1.5.4
+docker run -p 8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=vault' --cap-add=IPC_LOCK -d --name=dev-vault vault:1.2.4
