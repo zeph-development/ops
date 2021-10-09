1784ef7db304dc9c3bbc13f6e18bbefbcdc9b74a
WZHDAPG <pancudaniel7>
2021-10-09 13:21:16 +0300

    Added wget to osx init script

--- osx/install_packages.sh
+++ osx/install_packages.sh
@@ -5,12 +5,14 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew install tree;
 brew install watch;
+brew install wget;
 
 brew install --cask visual-studio-code;
 
 brew install python@3.8;
 brew install pyenv;
 pyenv install 3.9.2;
+python3 -m pip install --upgrade pip;
 
 brew install go;
 
