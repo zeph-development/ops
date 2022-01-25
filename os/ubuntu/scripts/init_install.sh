b730d8400844927143b5395efe42d4339f696ebc
Daniel <pancudaniel7@users.noreply.github.com>
2022-01-25 14:21:09 +0200

    Update init_install.sh
    - updated ubuntu init_install script

--- os/ubuntu/scripts/init_install.sh
+++ os/ubuntu/scripts/init_install.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-cli_tools_install() {
+install_tools() {
+sudo apt install -y curl;
+sudo apt install -y vim;
 sudo apt install -y git;
 sudo apt install -y tree;
 sudo apt install -y htop;
-sudo apt install -y zsh;
-sudo apt install -y diodon;
+sudo apt install -y xbindkeys;
+sudo snap install codium --classic;
 }
 
-sudo apt update &&
-cli_tools_install &&
+install_diodon() {
+sudo apt install -y diodon;
+cat <<EOT >> ~/.xbindkeys
+"/usr/bin/diodon"
+   Control + grave	
+EOT
+}
+
+install_zsh(){
+sudo apt install -y zsh &&
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-sed 's/plugins=(/plugins=(\nzsh-autosuggestions/g' .zshrc > ~/.zshrc &&
-sudo snap install codium --classic;
+sed 's/plugins=(/plugins=(\nzsh-autosuggestions\n/g' ~/.zshrc > ~/.zshrc;
+}
+
+install_github_cli() {
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null &&
+sudo apt update &&
+sudo apt install gh
+}
 
 
+
+sudo apt update &&
+install_tools &&
+install_zsh
+install_github_cli
+install_diodon
