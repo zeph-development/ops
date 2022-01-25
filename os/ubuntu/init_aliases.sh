bdfa584fcd97cf8b588684b1f5d830c4c43f2684
pancudaniel7 <pancudaniel7@gmail.com>
2022-01-25 20:15:16 +0200

    Removed aliases script

--- os/ubuntu/init_aliases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-add_aliases() {
-wget https://gist.githubusercontent.com/pancudaniel7/6aebf7055bd936ace2efb97733a64bc5/raw/01e3e0350c876e84725eaadb91dbe34eadd232a2/.bashrc -O ->> ~/.zshrc
-}
-
-add_aliases
