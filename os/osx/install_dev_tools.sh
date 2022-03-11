86a5a351bc8ffcac96a9e518da3ef572387b8257
Daniel <pancudaniel7@users.noreply.github.com>
2022-03-11 00:46:17 -0500

    Dev tools install

---
+++ os/osx/install_dev_tools.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Developer setup tools script
+
+install_tools () {
+brew install iterm2;
+brew install
+brew install jq;
+brew install yq;
+brew install tcptraceroute;
+brew install telnet;
+brew install htop;
+brew install wget;
+brew install git;
+brew install gh;
+brew install tree;
+brew install watch;
+brew install wget;
+brew install go;
+brew install --cask docker;
+brew install kubernetes-cli;
+}
+
+install_apps () {
+brew install firefox;
+brew install google-chrome;
+brew install visual-studio-code;
+}
+
+# start...
+#xcode-select --install ||
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+#echo 'export PATH=/opt/homebrew/bin/:$PATH' >> ~/.bashrc &&
+#source ~/.bashrc &&
+install_rools;
+install_apps;
